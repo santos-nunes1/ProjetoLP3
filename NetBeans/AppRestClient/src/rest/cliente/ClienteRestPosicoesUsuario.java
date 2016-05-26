9695b07188475184337a032054cdb6e7a39ff05f
santos-nunes1 <santos-nunes1@hotmail.com>
2016-05-26 17:12:03 -0300

    Aplicação Desktop Para teste de Web Service
    Ainda da parte 4. Ficou faltando subir no ultimo commit

---
+++ NetBeans/AppRestClient/src/rest/cliente/ClienteRestPosicoesUsuario.java
@@ -0,0 +1,35 @@
+package rest.cliente;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.HttpClients;
+
+public class ClienteRestPosicoesUsuario {
+
+    public static void main(String[] args) throws IOException {
+        HttpClient cliente = HttpClients.createDefault();
+        HttpGet httpget = new HttpGet("http://localhost:8080/AppFrontController/LP3Rest/lp3/posicoes/mackTest@mack.br");
+        HttpResponse response = cliente.execute(httpget);
+        System.out.println("----------------------------------------");
+        System.out.println(response.getStatusLine());
+        HttpEntity entity = response.getEntity();
+        if (entity != null) {
+            InputStream instream = entity.getContent();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(instream));
+            StringBuilder out = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                out.append(line);
+            }
+            System.out.println(out.toString());
+            reader.close();
+            instream.close();
+        }
+    }
+}
